25ab13e9eb8bba84d9772c9154d7a9eb6c9a3fec
Arthur Plebs <arthur.45@live.fr>
2023-11-29 10:13:56 +0100

    fix content-type

--- auth-service/src/server.py
+++ auth-service/src/server.py
@@ -5,8 +5,13 @@ from src.services.email_checking import *
 from flask_bcrypt import Bcrypt
 from src.controllers.auth_controller import auth_controller
 from src.setup import *
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 app.register_blueprint(auth_controller)
 app.config.from_pyfile("setup.py")
 
--- front/static/auth-api.js
+++ front/static/auth-api.js
@@ -1,17 +1,32 @@
 
-const baseUrl = "http://localhost:5000"
+const baseUrl = "http://localhost:5001"
 
-async function sendFormData(url, formData) {
-    const response = await fetch(baseUrl + url, {
+async function sendFormData(url, formData, multipart= false) {
+    const plainFormData = Object.fromEntries(formData.entries());
+    const formDataJsonString = JSON.stringify(plainFormData);
+
+    const fetchConfig= {
         method: 'POST',
-        body: formData
-    });
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: formDataJsonString
+    }
+
+    if (multipart) {
+        delete fetchConfig.headers["Content-Type"]
+        fetchConfig.body = formData
+    }
+
+    console.log(fetchConfig)
+
+    const response = await fetch(baseUrl + url, fetchConfig);
 
     if (!response.ok) {
         throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
-    return await response.json();
+    return response
 }
 async function signup(formData) {
     await sendFormData('/signup', formData)
@@ -26,9 +41,19 @@ async function resendCode(formData) {
 }
 
 async function getCertificates(formData) {
-    await sendFormData('/certificate', formData)
+    const res = await sendFormData('/certificate', formData)
+    const blob = await res.blob()
+    const fileUrl = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = fileUrl;
+    a.download = 'certificate.pem';
+    a.click();
+    a.remove()
+    return Promise.resolve()
 }
 
 async function login(formData) {
-    await sendFormData('/login', formData)
+    const res = await sendFormData('/login', formData, true)
+    // const data = await res.json()
+    // localStorage.setItem('token', data.access_token)
 }
